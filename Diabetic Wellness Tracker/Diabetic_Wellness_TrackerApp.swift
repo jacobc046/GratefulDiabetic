8bab00efeee49bdaf69a802959f0ae5979e355d7
Jacob Croket <thisisjacobseml@icloud.com>
2024-08-05 14:36:18 -0400

    Initial Commit

---
+++ Diabetic Wellness Tracker/Diabetic_Wellness_TrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  Diabetic_Wellness_TrackerApp.swift
+//  Diabetic Wellness Tracker
+//
+//  Created by Jacob Croket on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Diabetic_Wellness_TrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
