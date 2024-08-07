d4290bc7d9ab236467cead408b2af4e96f330da4
Jacob Croket <thisisjacobseml@icloud.com>
2024-08-07 14:40:36 -0400

    Fixed persistance issues

---
+++ Diabetic Wellness Tracker/Views/SearchJournals.swift
@@ -0,0 +1,18 @@
+//
+//  SearchJournals.swift
+//  Diabetic Wellness Tracker
+//
+//  Created by Jacob Croket on 8/7/24.
+//
+
+import SwiftUI
+
+struct SearchJournals: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchJournals()
+}
